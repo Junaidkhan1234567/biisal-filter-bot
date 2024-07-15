b8fb871ba6fdd6a4e792c784b41b0e82c9b30265
Prinal khan <145959127+Junaidkhan1234567@users.noreply.github.com>
2024-07-15 23:38:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 #main variables
 API_ID = int(environ.get('API_ID', '21606847'))
 API_HASH = environ.get('API_HASH', 'b9708b698eed88f053454aed2598e571')
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7298218067:AAEysUYx0DRECeCJisa2W-4DifI1n25zNTw')
 
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1383239349').split()]
 USERNAME = environ.get('USERNAME', "https://WOLVERINE_P")
